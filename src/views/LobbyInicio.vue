20138edcef2bf4967dbdedf23b319321632cd06a
Carlos Lopez <carlosedm97@gmail.com>
2025-04-14 13:00:04 -0500

    Agregado el logo de UNO a la vista

--- src/views/LobbyInicio.vue
+++ src/views/LobbyInicio.vue
@@ -1,13 +1,12 @@
+<script setup>
+import Logo from "../components/logo.vue";
+</script>
+
 <template>
   <div
     class="d-flex flex-column align-items-center justify-content-center min-vh-100 text-center px-3"
   >
-    <img
-      src="../assets/uno-logo.png"
-      alt="UNO Logo"
-      class="img-fluid mb-4"
-      style="max-width: 200px"
-    />
+    <Logo class="row logo-container mb-4" />
 
     <div class="d-grid gap-3 w-100" style="max-width: 300px">
       <button class="btn btn-outline-danger btn-lg">Crear sala</button>
@@ -18,6 +17,8 @@
   </div>
 </template>
 
-<script setup></script>
-
-<style scoped></style>
+<style scoped>
+.logo-container {
+  width: 40%;
+}
+</style>
